e4a43080f22d4d60da07c306560274082397a254
Maxpols <maxpoletiek@gmail.com>
2023-11-28 19:41:11 +0100

    Creating of class

---
+++ src/multiple_linear_regression.py
@@ -0,0 +1,10 @@
+class MultipleLinearRegression:
+
+    def train:
+        #This class should have a train method which calculates the parameters as from Equation (10) based on a given training dataset
+        # of observations and ground truth.
+            # – You can pick the format for the dataset you prefer; however,
+            # we suggest it to be encoded in a tabular/matrix form with the
+            # rows representing the n units and the columns representing
+            # the p features. Similarly, the ground truth should be a single
+            # columnm table/vector of size n.
